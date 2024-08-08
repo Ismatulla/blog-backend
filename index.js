af65025c1eb0d18e57330d46fe3d2919e0f6d9f8
Ismatulla <ismatilloqoyliyev@gmail.com>
2024-08-08 13:35:51 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+const MongoStore = require('connect-mongo')
+const blogRoutes = require('./routes/blogsRoute');
+const dbUrl = process.env.MONGODB_URI || 'mongodb://localhost:27017/blogpost';
+const secret = process.env.SECRET || 'secretforblogpost';
+
+mongoose.connect(dbUrl)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+const store = MongoStore.create({
+  mongoUrl: dbUrl,
+  secret,
+  touchAfter: 1000 * 60 * 60 * 24 * 7,
+})
+store.on('error', err => console.log(err));
+
+//middleware
+app.use(express.json());
+app.use(express.json())
+app.use(cors())
+app.use('/blogpost', blogRoutes);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port} 😀`);
+}); 
+
+
+
+
